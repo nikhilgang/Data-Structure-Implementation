815c13992dc944d362e2edb6212ea2cd591790ed
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2020-07-08 22:21:44 +0530

    Changing Case()

---
+++ String/Changing_Case.cpp
@@ -0,0 +1,28 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{   int i=0;
+    string str;
+    cout<<"enter a string   ";
+    cin>>str;
+    
+    while(str[i]!='\0')
+    {
+        
+        if(str[i]>='a' && str[i]<='z')
+        {
+            str[i]=str[i]-32;
+        }
+        else if(str[i]>='A' && str[i]<='Z')
+        {
+            str[i]=str[i]+32;
+        }
+        i++;
+    }
+    cout<<str;
+
+    return 0;
+}
