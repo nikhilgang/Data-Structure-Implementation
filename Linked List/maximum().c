ea687841e9da1f9909702dd96655f2b86247cda3
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2020-07-30 22:13:00 +0530

    maximum

---
+++ Linked List/maximum().c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+struct node 
+{
+    int data;
+    struct node *next;
+};
+
+struct node* create(int a[],int n)
+{
+    struct node *newnode,*head=NULL,*t;
+    int i;
+    for(i=0;i<n;i++)
+    {
+        newnode=(struct node*)malloc(sizeof(struct node));
+        if(head==NULL)
+        {
+            newnode->data=a[i];
+            newnode->next=NULL;
+            head=newnode;
+            t=head;
+        }
+        else
+        {
+            newnode->data=a[i];
+            newnode->next=NULL;
+            t->next=newnode;
+            t=t->next;
+        }
+    }
+    return head;
+}
+
+void display(struct node *p)
+{
+    while(p!=NULL)
+    {
+        printf("%d ",p->data);
+        p=p->next;
+    }
+}
+
+void max(struct node *p)
+{
+    int s=INT_MIN;
+    while(p!=NULL)
+    {
+        if(p->data>s)
+            s=p->data;
+        p=p->next;
+    }
+    printf("maximum is %d",s);
+}
+int main()
+{  struct node *p;
+   int a[]={1,2,3,4,5,6,7,8};
+   
+   p=create(a,8);
+   
+   display(p);
+   printf("\n");
+   max(p);
+
+    return 0;
+}
