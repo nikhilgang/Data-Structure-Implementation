26887d10b9e9f54fe31164000c5f29b2831ef5cb
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2020-08-29 23:02:18 +0530

    isFull

---
+++ Stack/isFull().c
@@ -0,0 +1,66 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int top;
+    int size;
+    int *S;
+};
+
+void create(struct stack *st)
+{
+    printf("enter size \n");
+    scanf("%d",&st->size);
+    st->top=-1; 
+    st->S=(int*)malloc(sizeof(int) * st->size);
+}
+
+void display(struct stack st)
+{
+    int i;
+    for(i=st.top;i>=0;i--)
+    {
+        printf("%d  ",st.S[i]);
+    }
+}
+
+int isEmpty(struct stack st)
+{
+    if(st.top==-1)
+        return 1;
+    return 0;
+}
+
+int isFull(struct stack st)
+{
+    if(st.top==st.size-1)
+        return 1;
+    return 0;
+}
+
+void push(struct stack *st,int ele)
+{
+    int i=0;
+    if(isFull(*st))
+        printf("Stack overflow ");
+    else
+        st->top++;
+        st->S[st->top]=ele;
+        
+}
+
+
+
+int main()
+{
+    struct stack st;
+    create(&st);
+    
+    push(&st,1);
+    push(&st,2);
+    
+    display(st);
+    return 0;
+}
