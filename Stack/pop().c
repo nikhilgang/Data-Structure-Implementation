ca67c76e004febd14e653d6194d6e5e18bcbf197
Nikhil Gangwar <47890912+nikhilgang@users.noreply.github.com>
2020-09-02 15:36:10 +0530

    pop

---
+++ Stack/pop().c
@@ -0,0 +1,91 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+
+struct stack
+{
+    int top;
+    int size;
+    int *S;
+};
+
+void create(struct stack *st)
+{
+    printf("enter size \n");
+    scanf("%d",&st->size);
+    st->top=-1; 
+    st->S=(int*)malloc(sizeof(int) * st->size);
+}
+
+void display(struct stack st)
+{
+    int i;
+    printf("final stack\n");
+    for(i=st.top;i>=0;i--)
+    {
+        printf("%d  ",st.S[i]);
+    }
+}
+
+int isEmpty(struct stack st)
+{
+    if(st.top==-1)
+        return 1;
+    return 0;
+}
+
+int isFull(struct stack st)
+{
+    if(st.top==st.size-1)
+        return 1;
+    return 0;
+}
+
+void push(struct stack *st,int ele)
+{
+    int i=0;
+    if(isFull(*st))
+        printf("Stack overflow ");
+    else
+        st->top++;
+        st->S[st->top]=ele;
+        
+}
+
+int pop(struct stack *st)
+{   
+    int element;
+    if(isEmpty(*st))
+    {
+        printf("Stack underflow ");
+        return -1;
+    }
+    else
+    {
+        element=st->top;
+        st->top--;
+        return element;
+    }
+}
+
+int stackTop(struct stack st)
+{
+    if(!isEmpty(st))
+        return st.S[st.top];
+    return -1;
+}
+
+int main()
+{
+    struct stack st;
+    create(&st);
+    
+    push(&st,1);
+    push(&st,2);
+    push(&st,4);
+
+    display(st);
+    
+    printf("\n%d",stackTop(st));
+    return 0;
+}
